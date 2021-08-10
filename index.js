77db2c4fc1595c5ab4efab83a678e1f6437e0dcb
Benis Tambe <will.jenifer@yahoo.com>
2021-08-09 22:08:36 -0400

    commit tree 4beebceaacf00801cec643a1676fda2c9167e72b

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, aKey, val) {
+  const newObj = Object.assign({}, driver);
+  newObj[aKey] = val;
+  // return Object.assign({}, driver, { [aKey]: val });
+  return newObj;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, aKey, val) {
+  driver[aKey] = val;
+  return driver;
+}
+
+function deleteFromDriverByKey(driverObj, aKey) {
+  const newObj = Object.assign({}, driverObj);
+  delete newObj[`${aKey}`];
+  return newObj;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, aKey, val) {
+  delete driver[`${aKey}`];
+  return driver;
+}
